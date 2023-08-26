52f9becee89d554da3ee2b4191b3e1bb13cf36cf
liangyuanyong <lyywenwen@proton.me>
2023-08-26 10:40:55 +0800

    update

--- src/views/main/appraisalMan/contentEdit.vue
+++ src/views/main/appraisalMan/contentEdit.vue
@@ -1,10 +1,10 @@
 <template>
   <h1>考核管理/考核内容编辑</h1>
   <div class="content">
-    <div class="add-button">
-      <router-link to="/main/contentEdit1" class="link">+添加</router-link>
-    </div>
-    <el-table style="width: 100%">
+    <button class="add-button">
+      <router-link to="/main/contentEdit1" class="link" >+添加</router-link>
+    </button>
+    <el-table style="width: 100%" class="el-table">
       <el-table-column label="标题" align="center">
         <template #default="scope">{{ scope.row.title }}</template>
       </el-table-column>
@@ -54,10 +54,21 @@ const handleDelete = (assessmentId) => {
 };
 </script>
 
-  
- 
-  
-  
-  <style>
+<style>
+.add-button{
+  background-color: rgba(55, 127, 127);
+  line-height: 40px;
+  text-align: center;
+  font-size: 18px;
+  border-radius: 10px;
+  width: 80px;
+  height: 40px;
+  border: 0px;
+  float: right;
+}
 
-  </style>
+.link{
+  color: #fff;
+}
+
+</style>
--- src/views/main/set/set.vue
+++ src/views/main/set/set.vue
@@ -1,16 +1,12 @@
 <template>
   <div class="set">
-<<<<<<< HEAD
     <div class="left">
-=======
-    <!-- 其他 Set 父组件内容 -->
-    <infoSet></infoSet>
->>>>>>> ba39cf98fb10fef9ce4143a25573dc005c0734ed
     <programShow></programShow>
     <team></team> 
     </div>
     <div class="right">
     <announceShow></announceShow>
+    <infoset></infoset>
     </div>
     
     
@@ -20,31 +16,25 @@
 
 <script>
 import Team from './team.vue';
-<<<<<<< HEAD
 import programShow from './programShow.vue'
-import announceShow from './announceShow.vue';
-=======
-import programShow from './programShow.vue'// 使用相对路径引入
-import infoSet from './infoSet.vue';
->>>>>>> ba39cf98fb10fef9ce4143a25573dc005c0734ed
+import  announceShow from './ announceShow.vue';
+import infoset from '.infoSet.vue'
+
 
 export default {
   components: {
     Team,
-<<<<<<< HEAD
     programShow, 
-    announceShow
-=======
-    programShow, // 注册子组件
-    infoSet,
->>>>>>> ba39cf98fb10fef9ce4143a25573dc005c0734ed
+    announceShow,
+    infoset
+
   },
 
 
 };
 </script>
 
-<<<<<<< HEAD
+
 <style>
 .set{
   height: 100%;
@@ -57,16 +47,6 @@ export default {
 }
 .right{
   width: 45%;
-=======
-<style scoped>
-.parent-component {
-  /* 其他样式属性 */
-  overflow: hidden; /* 隐藏父组件的滚动条 */
->>>>>>> ba39cf98fb10fef9ce4143a25573dc005c0734ed
 }
 
-.set{
-  width: 100%;
-  height: 100%;
-}
 </style>
